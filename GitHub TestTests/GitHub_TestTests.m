56e1c1740a1503c4dc25d3f4e650442a0ac4e630
Ma Yan <896098846@qq.com>
2014-06-05 10:54:41 +0900

    Initial Commit

---
+++ GitHub TestTests/GitHub_TestTests.m
@@ -0,0 +1,34 @@
+//
+//  GitHub_TestTests.m
+//  GitHub TestTests
+//
+//  Created by 馬 岩 on 14-6-5.
+//  Copyright (c) 2014年 馬 岩. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitHub_TestTests : XCTestCase
+
+@end
+
+@implementation GitHub_TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
